832e70107b21a4181252f4d131432b8e01d46bc9
sumin7928 <sumin7928@naver.com>
2022-05-13 15:28:01 +0900

    add - MIP721

---
+++ contracts/MIP721.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./MIP721Snapshot.sol";
+
+contract MIP721 is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, AccessControl, ERC721Burnable, MIP721Snapshot {
+    using Counters for Counters.Counter;
+
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    Counters.Counter private _tokenIdCounter;
+
+    string private _baseTokenURI;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory baseTokenURI
+    ) ERC721(name, symbol) {
+        _baseTokenURI = baseTokenURI;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setBaseURI(string memory baseUri) external onlyRole(ADMIN_ROLE) {
+        _baseTokenURI = baseUri;
+    }
+
+    function pause() public onlyRole(ADMIN_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(ADMIN_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, string memory uri) public onlyRole(ADMIN_ROLE) {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function snapshot(uint256 _id, uint256 _hash) public override onlyRole(ADMIN_ROLE) {
+        //require(ownerOf(_id) == _msgSender(), "not owner");
+        MIP721Snapshot._snapshot(_id, _hash);
+    }
+    
+    function removeSnapshot(uint256 _id) public onlyRole(ADMIN_ROLE) {
+        //require(ownerOf(_id) == _msgSender(), "not owner");
+        MIP721Snapshot._snapshot(_id, 0x0);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl, MIP721Snapshot)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ contracts/MIP721Snapshot.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+abstract contract MIP721Snapshot {
+    mapping(uint256 => uint256) private snapshortList;
+
+    event Snapshot(address indexed _operator, uint256 indexed _id, uint256 _hash);
+
+    function snapshot(uint256 _id, uint256 _hash) public virtual;
+
+    function _snapshot(uint256 _id, uint256 _hash) internal virtual {
+        // require(snapshortList[_id] == 0, "MIP721Snapshot: already snapshoted");
+        // require(_hash != 0, "MIP721Snapshot: _hash can not have 0");
+
+        if(_hash != 0) {
+            snapshortList[_id] = _hash;
+        } else {
+            delete snapshortList[_id];
+        }
+        
+        emit Snapshot(msg.sender, _id, _hash);
+    }
+
+    function snapshoted(uint256 _id) public view virtual returns (bool) {
+        return (snapshortList[_id] != 0);
+    }
+    function snapshotOf(uint256 _id) public virtual returns (uint256) {
+        return snapshortList[_id];
+    }
+
+    // /**
+    //  * @dev for ERC721
+    //  */
+    // function _beforeTokenTransfer(
+    //     address, // from,
+    //     address, // to,
+    //     uint256 tokenId
+    // ) internal virtual {
+    //     require(!snapshoted(tokenId), "MIP721Snapshot: token is locked");
+    // }
+
+    // /**
+    //  * @dev for ERC1155
+    //  */
+    // function _beforeTokenTransfer(
+    //     address, // operator,
+    //     address, // from,
+    //     address, // to,
+    //     uint256[] memory ids,
+    //     uint256[] memory, // amounts,
+    //     bytes memory // data
+    // ) internal virtual {
+    //     for (uint256 i = 0; i < ids.length; ++i) {
+    //         require(!snapshoted(ids[i]), "MIP721Snapshot: token(1155) is locked");
+    //     }
+    // }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
+        return interfaceId == type(MIP721Snapshot).interfaceId;
+    }
+}
