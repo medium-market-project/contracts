36e491a1049878d4910632aeea4b1290da1bf332
K <41320108+k-lyze@users.noreply.github.com>
2022-06-20 21:44:26 +0900

    Update ISwapFactory.sol

--- contracts/ISwapFactory.sol
+++ contracts/ISwapFactory.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.12;
 interface ISwapFactory {
     event CreateExchange(address indexed token, address indexed exchange);
 
-    function createExchange(address token, string calldata name, string calldata symbol) external returns (address);
+    function createExchange(address token) external returns (address);
     function getExchange(address token) external view returns (address);
     function getToken(address exchange) external view returns (address);
     function getTokenWithIdx(uint256 tokenIdx) external view returns (address);
