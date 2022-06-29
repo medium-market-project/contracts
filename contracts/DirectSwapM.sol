55348f8a1ee0fc2c52d8d6268d23279318a01308
K <41320108+k-lyze@users.noreply.github.com>
2022-06-29 16:20:05 +0900

    Update DirectSwapM.sol

--- contracts/DirectSwapM.sol
+++ contracts/DirectSwapM.sol
@@ -6,11 +6,17 @@ import "./MediumAccessControl.sol";
 import "./MediumPausable.sol";
 
 contract MediumSwapAgentM is MediumAccessControl, MediumPausable {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
 
     event WithdrawReserveM(address indexed admin, address indexed to, uint amount);
     event SwapInM(uint indexed swapKey, address indexed from, uint amount);
     event SwapOutM(uint indexed swapKey, address indexed to, uint amount);
     event SwapRefundM(uint indexed swapKey, address indexed to, uint amount);
+
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
+    }
 
     function withdrawReserve(address to, uint amount) external onlyAdmin {
         require (amount <= address(this).balance, "insufficient reserve");
