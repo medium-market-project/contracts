c12607bd7c866bc69b00b7a6fa130210e531146b
K <41320108+k-lyze@users.noreply.github.com>
2022-06-29 15:59:39 +0900

    Update DirectSwapM.sol

--- contracts/DirectSwapM.sol
+++ contracts/DirectSwapM.sol
@@ -7,22 +7,22 @@ import "./MediumPausable.sol";
 
 contract MediumSwapAgentM is MediumAccessControl, MediumPausable {
 
-    event SwapIn(address indexed from, uint amount);
-    event SwapOut(address indexed to, uint amount);
+    event SwapInM(uint indexed swapKey, address indexed from, uint amount);
+    event SwapOutM(uint indexed swapKey, address indexed to, uint amount);
 
     function withdrawReserve(address to, uint amount) external onlyAdmin {
         require (amount <= address(this).balance, "insufficient reserve");
         payable(to).transfer(amount);
     }
 
-    function swapIn() payable external whenNotPaused {
-        emit SwapIn(msg.sender, msg.value);
+    function swapIn(uint swapKey) payable external whenNotPaused {
+        emit SwapIn(swapKey, msg.sender, msg.value);
     }
 
-    function swapOut(address to, uint amount) external onlyAdmin {
+    function swapOut(uint swapKey, address to, uint amount) external onlyAdmin {
         require (to != address(0), "invalid address");
         require (amount <= address(this).balance, "insufficient reserve");
         payable(to).transfer(amount);
-        emit SwapOut(to, amount);
+        emit SwapOut(swapKey, to, amount);
     }
 }
